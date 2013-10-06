ed44c09b48f7b93eb35480343ab7ce8f89e6d87a
costrou <costrou@costrou-VirtualBox.(none)>
2013-10-05 19:10:38 -0500

    made major changes

--- html/js/app.js
+++ html/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.ace']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
     config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 	    when('/home', {templateUrl: 'partials/home.html', controller: homeController}).
