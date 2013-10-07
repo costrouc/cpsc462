b5ff03e9b763fa75b7413ceaa9cf6014401609fe
chris ostrouchov <chris.ostrouchov@gmail.com>
2013-10-07 12:03:27 -0400

    Added anchor routing logic. Modified index.html to point to projects

--- html/js/app.js
+++ html/js/app.js
@@ -1,12 +1,21 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.ace']).
-    config(['$routeProvider', function($routeProvider) {
-	$routeProvider.
-	    when('/home', {templateUrl: 'partials/home.html', controller: homeController}).
-	    when('/about', {templateUrl: 'partials/about.html', controller: homeController}).
-	    when('/projects', {templateUrl: 'partials/projects.html', controller: homeController}).
-	    when('/blog', {templateUrl: 'blog.chrisostrouchov.html'}).
-	    otherwise({redirectTo: '/home'});
-    }]);
+
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+	when('/home', {templateUrl: 'partials/home.html', controller: homeController}).
+	when('/about', {templateUrl: 'partials/about.html', controller: homeController}).
+	when('/projects', {templateUrl: 'partials/projects.html', controller: homeController}).
+	when('/blog', {templateUrl: 'blog.chrisostrouchov.html'}).
+	otherwise({redirectTo: '/home'});
+}]);
+
+myApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
+    $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+	$location.hash($routeParams.scrollTo);
+	$anchorScroll();
+     });
+});
